68422079a7ea2a6bbeb66840700c7aa47514d548
V <veronika.m.winters@gmail.com>
2024-08-23 14:48:49 -0700

    nested

--- src/db.rs
+++ src/db.rs
@@ -1,49 +1,3 @@
-use diesel::prelude::*;
-use rocket_sync_db_pools::database;
-
-#[database("diesel")]
-struct Db(diesel::SqliteConnection);
-
-#[derive(serde::Serialize, Queryable)]
-#[diesel(table_name = posts)]
-struct Post {
-    #[serde(skip_serializing)]
-    id: Option<i32>,
-    title: String,
-    body: String,
-    author: String,
-}
-
-table! {
-    posts (id) {
-        id -> Nullable<Integer>,
-        title -> Text,
-        body -> Text,
-        author -> Text,
-    }
-}
-
-#[derive(serde::Serialize, Queryable)]
-#[diesel(table_name = comments)]
-struct Comment {
-    #[serde(skip_serializing)]
-    id: Option<i32>,
-    author: String,
-    body: String,
-    parent_id: Option<i32>,
-    post_id: i32,
-}
-
-table! {
-    comments (id) {
-        id -> Nullable<Integer>,
-        author -> Text,
-        body -> Text,
-        parent_id -> Nullable<Integer>,
-        post_id -> Integer
-    }
-}
-
 // fn new_post() -> Post {
 //     let comment = Comment {
 //         author: String::from("me"),
@@ -62,8 +16,3 @@ table! {
 //     }
 // }
 //
-
-async fn get_post(db: Db, id: i32) -> Result<Post, diesel::result::Error> {
-    db.run(move |conn| posts::table.filter(posts::id.eq(id)).first(conn))
-        .await
-}
--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,106 @@
 extern crate rocket;
 use rocket::http::{CookieJar, Status};
 use rocket::response::{Flash, Redirect};
+use rocket_db_pools::sqlx::sqlite::SqliteRow;
 use rocket_dyn_templates::{context, tera::Tera, Template};
 // use serde::Serialize;
+use rocket_db_pools::sqlx::{self, Row};
+use rocket_db_pools::{Connection, Database};
+use serde::Deserialize;
+use std::borrow::Cow;
 
-mod db;
+#[derive(Database)]
+#[database("main")]
+struct Main(sqlx::SqlitePool);
+
+#[derive(sqlx::FromRow, Debug, serde::Serialize)]
+struct Post {
+    id: i32,
+    title: String,
+    body: String,
+    author: String,
+    #[sqlx(skip)]
+    comments: Vec<Comment>,
+}
+
+#[derive(sqlx::FromRow, Debug, serde::Serialize, Clone)]
+struct Comment {
+    id: i32,
+    author: String,
+    body: String,
+    parent_id: Option<i32>,
+    post_id: i32,
+    #[sqlx(skip)]
+    children: Vec<Comment>,
+}
+
+fn sort_comments(comments: Vec<Comment>) -> Vec<Comment> {
+    let backup = comments.clone();
+    let parent_comments: Vec<Comment> = comments
+        .into_iter()
+        .filter(|c| c.parent_id == None)
+        .collect();
+    let child_comments: Vec<Comment> = backup.into_iter().filter(|c| c.parent_id != None).collect();
+
+    parent_comments
+        .into_iter()
+        .map(|mut parent| {
+            let possible = child_comments.clone();
+            let children = children_for_parent(&parent, possible);
+            parent.children = children;
+            parent
+        })
+        .collect()
+}
+
+fn children_for_parent(comment: &Comment, possible: Vec<Comment>) -> Vec<Comment> {
+    possible
+        .into_iter()
+        .filter(|child| match child.parent_id {
+            Some(parent_id) => {
+                if parent_id == comment.id {
+                    true
+                } else {
+                    false
+                }
+            }
+
+            None => false,
+        })
+        .collect()
+}
 
 #[get("/")]
-fn index(jar: &CookieJar<'_>) -> Template {
+async fn index(mut db: Connection<Main>, jar: &CookieJar<'_>) -> Template {
     match jar.get_private("user_id") {
-        Some(c) => Template::render(
-            "index",
-            context! {
-                name: c.value(),
-
-            },
-        ),
+        Some(c) => {
+            let p = sqlx::query_as::<_, Post>("select * from posts where id = ?")
+                .bind(1)
+                .fetch_one(&mut **db)
+                .await;
+            let co = sqlx::query_as::<_, Comment>("select * from comments where post_id = ?")
+                .bind(1)
+                .fetch_all(&mut **db)
+                .await
+                .unwrap();
+            match p {
+                Ok(mut post) => {
+                    post.comments = sort_comments(co);
+                    println!("[DATA]:: {:#?}", post);
+                    Template::render(
+                        "index",
+                        context! {
+                            name: c.value(),
+                            post: post
+                        },
+                    )
+                }
+                Err(d) => {
+                    println!("[ERROR]:: {}", d);
+                    Template::render("login", context! {})
+                }
+            }
+        }
         None => Template::render("login", context! {}),
     }
 }
@@ -50,4 +135,5 @@ fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, post_login, logout])
         .attach(Template::fairing())
+        .attach(Main::init())
 }
