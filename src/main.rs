0f03a9b7880b48a83ea4aa1ce978fba3195524de
V <veronika.m.winters@gmail.com>
2024-08-22 20:40:35 -0700

    first

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket_dyn_templates::{context, tera::Tera, Template};
+
+#[get("/")]
+fn index() -> &'static str {
+    "hello worlds"
+}
+
+#[get("/greeter/<name>")]
+fn greeter(name: &str) -> Template {
+    Template::render(
+        "index",
+        context! {
+            name: Some(name)
+        },
+    )
+}
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, greeter])
+}
