dd22ec7df4277cdb1779ae67556decb6cc68c168
V <veronika.m.winters@gmail.com>
2024-08-22 23:58:27 -0700

    route refactor

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,45 @@ use rocket::response::{Flash, Redirect};
 use rocket_dyn_templates::{context, tera::Tera, Template};
 
 #[get("/")]
-fn index(jar: &CookieJar<'_>) -> Result<Redirect, Template> {
+fn index(jar: &CookieJar<'_>) -> Template {
     match jar.get_private("user_id") {
-        Some(c) => Ok(Redirect::to(uri!(greeter))),
-        None => Err(Template::render("login", context! {})),
+        Some(c) => Template::render(
+            "index",
+            context! {
+                name: c.value()
+            },
+        ),
+        None => Template::render("login", context! {}),
     }
 }
 
 #[post("/")]
 fn post_login(jar: &CookieJar<'_>) -> Redirect {
     jar.add_private(("user_id", "admin"));
-    Redirect::to(uri!(greeter))
+    Redirect::to(uri!(index))
 }
 
-#[get("/greeter")]
-fn greeter(jar: &CookieJar<'_>) -> Result<Template, Redirect> {
-    match jar.get_private("user_id") {
-        Some(c) => Ok(Template::render(
-            "index",
-            context! {
-                name: c.value()
-            },
-        )),
-        None => Err(Redirect::to(uri!(index))),
-    }
-}
+// #[get("/greeter")]
+// fn greeter(jar: &CookieJar<'_>) -> Result<Template, Redirect> {
+//     match jar.get_private("user_id") {
+//         Some(c) => Ok(Template::render(
+//             "index",
+//             context! {
+//                 name: c.value()
+//             },
+//         )),
+//         None => Err(Redirect::to(uri!(index))),
+//     }
+// }
 
-#[post("/greeter")]
-fn post_greeter(jar: &CookieJar<'_>) -> Redirect {
+#[post("/logout")]
+fn logout(jar: &CookieJar<'_>) -> Redirect {
     jar.remove_private("user_id");
-    Redirect::to(uri!(greeter))
+    Redirect::to(uri!(index))
 }
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, greeter, post_login, post_greeter])
+        .mount("/", routes![index, post_login, logout])
         .attach(Template::fairing())
 }
